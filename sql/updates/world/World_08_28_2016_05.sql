675c0cd7733facb747c82609d7e51c72425ff59c
cybrann <trulex8@gmail.com>
2016-08-29 18:00:26 +0200

    Core/Custom Fixed some typos and removed custom spell script system

--- sql/updates/world/World_08_28_2016_05.sql
+++ sql/updates/world/World_08_28_2016_05.sql
@@ -1,3 +1,3 @@
-INSERT INTO `world`.`spell_proc_event` (`entry`, `Cooldown`) VALUES (146197, 18);
-INSERT INTO `world`.`spell_proc_event` (`entry`, `Cooldown`) VALUES (146198, 18);
-INSERT INTO `world`.`spell_proc_event` (`entry`, `Cooldown`) VALUES (146195, 18);
+INSERT INTO `spell_proc_event` (`entry`, `Cooldown`) VALUES (146197, 18);
+INSERT INTO `spell_proc_event` (`entry`, `Cooldown`) VALUES (146198, 18);
+INSERT INTO `spell_proc_event` (`entry`, `Cooldown`) VALUES (146195, 18);
