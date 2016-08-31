46d23e3e8d63410deda24d5e907513c36b451ae7
cooler-SAI <cooler19860212@yahoo.com>
2016-08-31 18:59:32 +0300

    DB/SAI: fixed all spells "on Close" target types

--- sql/SmartAI/patch/Development/Dungeons/Cataclysm/Grim Batol.sql
+++ sql/SmartAI/patch/Development/Dungeons/Cataclysm/Grim Batol.sql
@@ -187,7 +187,7 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,6,2000,4500,12000,14000,11,76507,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Claw Puncture'),
 (@ENTRY,0,1,0,6,0,100,7,0,0,0,0,11,74699,3,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Modgud\'s Malice on Death'),
-(@ENTRY,0,2,0,9,0,100,6,0,8,15000,18000,11,74837,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Modgud\'s Malady on Close');
+(@ENTRY,0,2,0,9,0,100,6,0,8,15000,18000,11,74837,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Modgud\'s Malady on Close');
 
 -- Trogg Dweller
 SET @ENTRY := 45467;
@@ -196,7 +196,7 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,6,2000,4500,12000,14000,11,76507,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Claw Puncture'),
 (@ENTRY,0,1,0,6,0,100,7,0,0,0,0,11,74699,3,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Modgud\'s Malice on Death'),
-(@ENTRY,0,2,0,9,0,100,6,0,8,15000,18000,11,74837,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Modgud\'s Malady on Close');
+(@ENTRY,0,2,0,9,0,100,6,0,8,15000,18000,11,74837,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Modgud\'s Malady on Close');
 
 -- Trogg Dweller
 SET @ENTRY := 39450;
--- sql/SmartAI/patch/Development/Locations/Eastern Kingdoms/Kelp'thar Forest.sql
+++ sql/SmartAI/patch/Development/Locations/Eastern Kingdoms/Kelp'thar Forest.sql
@@ -166,7 +166,7 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,2,0,100,0,0,70,15000,17000,11,80522,2,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Demoralizing Mmmrrrggglll at 70% HP'),
-(@ENTRY,0,1,0,9,0,100,0,0,20,15000,25000,11,6533,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Net on Close');
+(@ENTRY,0,1,0,9,0,100,0,0,20,15000,25000,11,6533,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Net on Close');
 
 -- Slickskin Eel
 SET @ENTRY := 41002;
