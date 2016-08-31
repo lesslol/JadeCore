40fe2e4b2395472702fc507ffb61f9fa8abbf4c1
cooler-SAI <cooler19860212@yahoo.com>
2016-08-31 21:16:21 +0300

    forgot GNU General Public License

--- sql/SmartAI/patch/Development/Locations/Eastern Kingdoms/Shimmering Expanse.sql
+++ sql/SmartAI/patch/Development/Locations/Eastern Kingdoms/Shimmering Expanse.sql
@@ -1,3 +1,23 @@
+/* Copyright (C) 11 April 2011-2016 patch Project 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public Licensefor more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/> 
+ *
+ * patch Project  Copyright (C) 11 April 2011-2014 patch Project 
+ * This program comes with ABSOLUTELY NO WARRANTY;
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions.
+ */
+ 
+
 -- Agitated Green Sand Crab
 SET @ENTRY := 40238;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
