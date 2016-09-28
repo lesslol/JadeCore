e25c40f89b700a68034307a99d1fcbbcd24f976e
cooler-SAI <cooler19860212@yahoo.com>
2016-09-28 19:52:37 +0300

    Quick Fix

--- sql/updates/world/5.4.7/2016_09_28_01_world.sql
+++ sql/updates/world/5.4.7/2016_09_28_01_world.sql
@@ -1,2 +1,2 @@
-UPDATE `smart_scripts` SET `event_flags` = 0 WHERE `id` IN (2,6);
-UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` IN (3,7);
+UPDATE `smart_scripts` SET `event_flags` = 0 WHERE `event_flags` IN (2,6);
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `event_flags` IN (3,7);
