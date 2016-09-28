a5c690eee49c59eb77984236f8b18a52b49c55de
cooler-SAI <cooler19860212@yahoo.com>
2016-09-28 18:28:27 +0300

    Typo

--- sql/updates/world/5.4.7/2016_09_28_01_world.sql
+++ sql/updates/world/5.4.7/2016_09_28_01_world.sql
@@ -1,2 +1,2 @@
 UPDATE `smart_scripts` SET `event_flags` = 0 WHERE `id` IN (2,6);
-UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` IN (3,5);
+UPDATE `smart_scripts` SET `event_flags` = 1 WHERE `id` IN (3,7);
