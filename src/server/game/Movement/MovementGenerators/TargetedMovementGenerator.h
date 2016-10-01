939bc324dd1e7757c4b00beae2035eec46519728
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-01 02:57:27 -0400

    Core/MMAPS use mmaps in movement generators @Keital Thanks
    Signed-off-by: Bodeguero <joaocarlos.jc.souza@gmail.com>

--- src/server/game/Movement/MovementGenerators/TargetedMovementGenerator.h
+++ src/server/game/Movement/MovementGenerators/TargetedMovementGenerator.h
@@ -23,6 +23,7 @@
 #include "FollowerReference.h"
 #include "Timer.h"
 #include "Unit.h"
+#include "PathGenerator.h"
 
 class TargetedMovementGeneratorBase
 {
@@ -38,12 +39,12 @@ class TargetedMovementGeneratorMedium : public MovementGeneratorMedium< T, D >, 
 {
     protected:
         TargetedMovementGeneratorMedium(Unit &target, float offset, float angle) :
-            TargetedMovementGeneratorBase(target), i_recheckDistance(0),
+            TargetedMovementGeneratorBase(target), i_recheckDistance(0), i_path(NULL),
             i_offset(offset), i_angle(angle),
             i_recalculateTravel(false), i_targetReached(false)
         {
         }
-        ~TargetedMovementGeneratorMedium() {}
+        ~TargetedMovementGeneratorMedium() { delete i_path; }
 
     public:
         bool Update(T &, const uint32 &);
@@ -52,6 +53,7 @@ class TargetedMovementGeneratorMedium : public MovementGeneratorMedium< T, D >, 
         void unitSpeedChanged() { i_recalculateTravel=true; }
         void UpdateFinalDistance(float fDistance);
 
+        bool IsReachable() const { return (i_path) ? (i_path->getPathType() & PATHFIND_NORMAL) : true; }
     protected:
         void _setTargetLocation(T &);
 
@@ -60,6 +62,7 @@ class TargetedMovementGeneratorMedium : public MovementGeneratorMedium< T, D >, 
         float i_angle;
         bool i_recalculateTravel : 1;
         bool i_targetReached : 1;
+        PathGenerator* i_path;
 };
 
 template<class T>
