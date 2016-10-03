b01dcb60d6832aa0f1f6b78a2be91691f603c4ba
Keital <adrianrp1988@gmail.com>
2016-10-03 00:00:20 -0400

    Core/MMAPS Implement mmap commands

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -85,6 +85,7 @@ void AddSC_tele_commandscript();
 void AddSC_ticket_commandscript();
 void AddSC_titles_commandscript();
 void AddSC_wp_commandscript();
+void AddSC_mmaps_commandscript();
 
 #ifdef SCRIPTS
 //world
