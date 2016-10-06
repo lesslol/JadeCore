9b42abbc7e847f854fc1dcdc062459624c82ece1
Ice.Net <mysterium@ice-wow.com>
2016-10-06 17:09:25 +0200

    Update 2016_10_06_00_world.sql

--- sql/updates/world/5.4.7/2016_10_06_00_world.sql
+++ sql/updates/world/5.4.7/2016_10_06_00_world.sql
@@ -1,11 +1,11 @@
 --Fix Only DK Worgen start with Viciousness spell
 DELETE FROM playercreateinfo_spell WHERE race=22 AND Spell=68975
 INSERT INTO playercreateinfo_spell(race, class, Spell, Note)VALUES
-(22, 6, 68975, "Viciousness");
-(22, 1, 68975, "Viciousness");
-(22, 3, 68975, "Viciousness");
-(22, 4, 68975, "Viciousness");
-(22, 5, 68975, "Viciousness");
-(22, 8, 68975, "Viciousness");
-(22, 9, 68975, "Viciousness");
+(22, 6, 68975, "Viciousness"),
+(22, 1, 68975, "Viciousness"),
+(22, 3, 68975, "Viciousness"),
+(22, 4, 68975, "Viciousness"),
+(22, 5, 68975, "Viciousness"),
+(22, 8, 68975, "Viciousness"),
+(22, 9, 68975, "Viciousness"),
 (22, 11, 68975, "Viciousness");
